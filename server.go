6132434d0e6a41fe81ebd219a61aadb259d3edf8
fujisakikenya <fujisaki.k@dentsudigital.co.jp>
2022-12-16 19:25:11 +0900

    add CORS setting

--- server.go
+++ server.go
@@ -1,28 +1,62 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	"os"
+
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/kenya6565/graphql_go/graph"
+
+	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/go-chi/chi"
+	"github.com/gorilla/websocket"
+	"github.com/rs/cors"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	router := chi.NewRouter()
+
+	// Add CORS middleware around every request
+	// See https://github.com/rs/cors for full option listing
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	srv.AddTransport(&transport.Websocket{
+        Upgrader: websocket.Upgrader{
+            CheckOrigin: func(r *http.Request) bool {
+                // Check against your desired domains here
+                 return r.Host == "example.org"
+            },
+            ReadBufferSize:  1024,
+            WriteBufferSize: 1024,
+        },
+    })
+	// http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	// http.Handle("/query", srv)
+	
+	// /にアクセスするとplaygroundの処理
+	router.Handle("/", playground.Handler("Starwars", "/query"))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	// /queryにアクセスするとgraphqlサーバーの処理
+	router.Handle("/query", srv)
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	// log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	// log.Fatal(http.ListenAndServe(":"+port, nil))
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		panic(err)
+	}
 }
